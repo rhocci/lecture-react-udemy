c7e2fb5c8d720c86474d157148096b090a7e8b7f
rhocci <4tres1@gmail.com>
2025-08-13 01:28:29 +0900

    chore: 03 - 컴포넌트 파일 분리

--- course/03 React Essentials/01-starting-project/src/App.jsx
+++ course/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.alt}></img>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import CoreConcept from './components/CoreConcept';
+import Header from './components/Header';
 
 function App() {
   return (

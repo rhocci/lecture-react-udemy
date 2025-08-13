da9f2184f2b61a181335fcb17f497819c33fe81d
rhocci <4tres1@gmail.com>
2025-08-13 12:39:19 +0900

    learn: 03 - Event Prop

--- course/03 React Essentials/01-starting-project/src/components/TabButton/TabButton.jsx
+++ course/03 React Essentials/01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,9 +1,13 @@
 import './TabButton.css';
 
 export default function TabButton(props) {
+  function handleClick() {
+    console.log('Hello World!');
+  }
+
   return (
     <li>
-      <button>{props.children}</button>
+      <button onClick={handleClick}>{props.children}</button>
     </li>
   );
 }

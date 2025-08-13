2917999b61a3fd4b73998a4cee8b16dd73890710
rhocci <4tres1@gmail.com>
2025-08-14 00:36:54 +0900

    learn: 03 - Rendering Contents Conditionally(var)

--- course/03 React Essentials/01-starting-project/src/App.jsx
+++ course/03 React Essentials/01-starting-project/src/App.jsx
@@ -12,6 +12,20 @@ function App() {
     setSelectedTopic(selectedButton);
   }
 
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -34,17 +48,8 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-          {!selectedTopic && <p>Please select a topic.</p>}
         </section>
+        {tabContent}
       </main>
     </div>
   );

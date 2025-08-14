aa962ba86b6ec9fdac15193de3e47370dc493add
rhocci <4tres1@gmail.com>
2025-08-14 20:09:54 +0900

    learn: 04 - Dynamic Types

--- course/04 Essentials Deep Dive/01-starting-project/src/components/Examples/Examples.jsx
+++ course/04 Essentials Deep Dive/01-starting-project/src/components/Examples/Examples.jsx
@@ -29,6 +29,7 @@ export default function Examples() {
   return (
     <Section id="examples" title="Examples">
       <Tabs
+        buttonsContainer="menu"
         buttons={
           <>
             <TabButton

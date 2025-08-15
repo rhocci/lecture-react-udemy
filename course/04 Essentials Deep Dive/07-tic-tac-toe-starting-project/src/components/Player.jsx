6042fd7303d9f6068fb6bce6435ebdfd8c6eb994
rhocci <4tres1@gmail.com>
2025-08-15 23:08:14 +0900

    learn: 04 - handle edit Button

--- course/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ course/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -3,16 +3,20 @@ import { useState } from 'react';
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
-  const handleEditClick = () => setIsEditing(!isEditing);
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = isEditing ? (
+    <input type="text" required/>
+  ) : (
+    <span className="player-name">{name}</span>
+  );
 
   return (
     <li>
       <span className="player">
-        {isEditing ? (
-          <input type="text" />
-        ) : (
-          <span className="player-name">{name}</span>
-        )}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>

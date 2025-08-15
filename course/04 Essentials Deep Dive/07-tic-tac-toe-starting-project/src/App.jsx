08fbb9a4fa48cc6871905c93dcbaa648d7e091b0
rhocci <4tres1@gmail.com>
2025-08-15 22:41:23 +0900

    learn: 04 - Start tic-tac-toe project, Create Player Component

--- course/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ course/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,18 @@
+import Player from './components/Player.jsx';
+
 function App() {
-  return <p>Coming Soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAMEBOARD
+      </div>
+      LOGS
+    </main>
+  );
 }
 
 export default App;

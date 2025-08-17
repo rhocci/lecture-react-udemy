5f17ba5ca5632be5c03f8525b4021a859753a5e4
rhocci <4tres1@gmail.com>
2025-08-17 23:23:57 +0900

    learn: 04 - Outsourcing Datas

--- course/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ course/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
+import { WINNING_COMBINATIONS } from './winnig-combinations.js';
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';

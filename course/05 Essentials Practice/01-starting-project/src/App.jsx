a2bd99d890a45487cd7987faed8e013bf9c7ae92
rhocci <4tres1@gmail.com>
2025-08-20 23:40:37 +0900

    exercise: 05 - UserInput 컴포넌트 구현(상태/핸들러 수정 필요)

--- course/05 Essentials Practice/01-starting-project/src/App.jsx
+++ course/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import Header from './components/Header/Header.jsx';
+import UserInput from './components/UserInput/UserInput.jsx';
 
 function App() {
+  const [result, setResult] = useState([]);
+
+  function handleUserInput({
+    initialInvestment,
+    annualInvestment,
+    expectedReturn,
+    duration,
+  }) {
+    setResult((prevResult) => {
+      const updatedResult = [
+        ...prevResult,
+        { initialInvestment, annualInvestment, expectedReturn, duration },
+      ];
+
+      return updatedResult;
+    });
+  }
+
   return (
     <>
       <Header />
+      <UserInput onChange={handleUserInput} />
     </>
   );
 }
--- course/05 Essentials Practice/01-starting-project/src/components/UserInput/UserInput.jsx
+++ course/05 Essentials Practice/01-starting-project/src/components/UserInput/UserInput.jsx
@@ -1,1 +1,59 @@
 import './UserInput.css';
+
+const initialValue = {
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10,
+};
+
+export default function UserInput({ onChange }) {
+  return (
+    <form id="user-input">
+      <div className="input-group">
+        <label>
+          INITIAL INVESTMENT
+          <input
+            type="number"
+            name="initialInvestment"
+            onChange={(e) =>
+              onChange({ ...initialValue, initialInvestment: e.target.value })
+            }
+          />
+        </label>
+        <label>
+          ANNUAL INVESTMENT
+          <input
+            type="number"
+            name="annualInvestment"
+            onChange={(e) =>
+              onChange({ ...initialValue, annualInvestment: e.target.value })
+            }
+          />
+        </label>
+      </div>
+      <div className="input-group">
+        <label>
+          EXPECTED RETURN
+          <input
+            type="number"
+            name="expectedReturn"
+            onChange={(e) =>
+              onChange({ ...initialValue, expectedReturn: e.target.value })
+            }
+          />
+        </label>
+        <label>
+          DURATION
+          <input
+            type="number"
+            name="duration"
+            onChange={(e) =>
+              onChange({ ...initialValue, duration: e.target.value })
+            }
+          />
+        </label>
+      </div>
+    </form>
+  );
+}

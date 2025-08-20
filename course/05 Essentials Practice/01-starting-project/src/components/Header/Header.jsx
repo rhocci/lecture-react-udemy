36c245ddd7caae31a1476caf716a9977b006f764
rhocci <4tres1@gmail.com>
2025-08-20 23:05:20 +0900

    exercise: 05 - Header 컴포넌트 제작

--- course/05 Essentials Practice/01-starting-project/src/components/Header/Header.jsx
+++ course/05 Essentials Practice/01-starting-project/src/components/Header/Header.jsx
@@ -1,1 +1,11 @@
+import logoImg from '../../assets/investment-calculator-logo.png';
 import './Header.css';
+
+export default function Header() {
+  return (
+    <header id="header">
+      <img src={logoImg} alt="investment calculator logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}

ed3d3acecec799537f36865ac84ad928af9be18f
rhocci <4tres1@gmail.com>
2025-08-22 22:27:21 +0900

    solution: 05 - Calculate Results Data & Proper Way to Handle numbers

--- course/05 Essentials Practice/02-solution/src/components/Results.jsx
+++ course/05 Essentials Practice/02-solution/src/components/Results.jsx
@@ -1,3 +1,7 @@
+import { calculateInvestmentResults } from '../util/investment.js';
+
 export default function Results({ userInput }) {
+  const resultsData = calculateInvestmentResults(userInput);
+
   return <p>Results...</p>;
 }

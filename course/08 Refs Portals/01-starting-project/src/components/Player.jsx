e9300089da234cfd590b1f81a91f37b8cdda282e
rhocci <4tres1@gmail.com>
2025-08-27 21:03:22 +0900

    learn: 08 - Refs VS State

--- course/08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ course/08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -3,7 +3,7 @@ import { useState, useRef } from 'react';
 export default function Player() {
   const playerName = useRef();
 
-  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);

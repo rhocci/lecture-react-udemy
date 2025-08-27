100cf7251b2c738aa81c064446bc640986d3336e
rhocci <4tres1@gmail.com>
2025-08-27 21:36:40 +0900

    learn: 08 - Using Refs as variable

--- course/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ course/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
 
     setTimerStarted(true);
   }
 
-  function handleStop() {}
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
 
   return (
     <section className="challenge">
@@ -21,7 +25,7 @@ export default function TimerChallenge({ title, targetTime }) {
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
-      <button onClick={handleStart}>
+      <button onClick={timerStarted ? handleStop : handleStart}>
         {timerStarted ? 'Stop' : 'Start'} Challenge
       </button>
       <p className={timerStarted ? 'active' : undefined}>

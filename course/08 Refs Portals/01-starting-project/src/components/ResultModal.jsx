980eb6024d4e4edfeb11c7102152c1b47864bc49
rhocci <4tres1@gmail.com>
2025-08-29 00:17:52 +0900

    learn: 08 - Passing Refs by forwardRef

--- course/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
+++ course/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,4 +1,9 @@
-export default function ResultModal({ ref, result, targetTime }) {
+import { forwardRef } from 'react';
+
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
   return (
     <dialog ref={ref} className="result-modal">
       <h2>Your {result}</h2>
@@ -13,4 +18,6 @@ export default function ResultModal({ ref, result, targetTime }) {
       </form>
     </dialog>
   );
-}
+});
+
+export default ResultModal;

3b5148c4682db5989494e614c25586b83c7170a1
rhocci <4tres1@gmail.com>
2025-08-29 00:13:43 +0900

    learn: 08 - Passing Refs to Custom Components

--- course/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ course/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -3,6 +3,7 @@ import ResultModal from './ResultModal.jsx';
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -10,6 +11,7 @@ export default function TimerChallenge({ title, targetTime }) {
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -17,11 +19,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
   function handleStop() {
     clearTimeout(timer.current);
+    dialog.current.showModal();
   }
 
   return (
     <>
-      {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
         {timerExpired && <p>You lost!</p>}

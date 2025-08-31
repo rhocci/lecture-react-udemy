7ca7ec54469016ca9890cde3364a3b1799100fc8
rhocci <4tres1@gmail.com>
2025-08-31 17:33:18 +0900

    learn: 10 - useReducer

--- course/10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
+++ course/10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import Product from './components/Product.jsx';
 import CartContextProvider from './store/shopping-cart-context.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
 
 function App() {
   return (

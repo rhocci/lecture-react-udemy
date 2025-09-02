17ae87066d8cb0fadaa29f46aec2d7bb5bea31ee
rhocci <4tres1@gmail.com>
2025-09-02 11:48:57 +0900

    learn: 11 - Introducing Side Effects

--- course/11 Side Effects useEffect/01-starting-project/src/App.jsx
+++ course/11 Side Effects useEffect/01-starting-project/src/App.jsx
@@ -5,11 +5,21 @@ import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const sortedPlaces = sortPlacesByDistance(
+      AVAILABLE_PLACES,
+      position.coords.latitude,
+      position.coords.longitude
+    );
+    console.log(sortedPlaces);
+  });
 
   function handleStartRemovePlace(id) {
     modal.current.open();

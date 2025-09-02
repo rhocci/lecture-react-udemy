0f02992d40f4b3d7dac159fc354748bc0edf0040
rhocci <4tres1@gmail.com>
2025-09-02 23:24:17 +0900

    learn: 11 - Another Use Case of Cleanup Function

--- course/11 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
+++ course/11 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,11 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  useEffect(() => {
-    const timer = setTimeout(() => onConfirm(), 3000);
+  const [remainingTime, setRemainingTime] = useState(TIMER);
 
-    return () => clearTimeout(timer);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [onConfirm]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div id="delete-confirmation">
@@ -19,6 +37,8 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
+      <p>{(remainingTime / 1000).toFixed(0)} seconds left...</p>
     </div>
   );
 }

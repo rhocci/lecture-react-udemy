06aff73975159f2bcd16948f93f044e42037dfcb
rhocci <4tres1@gmail.com>
2025-09-18 17:18:56 +0900

    solution: 12 - Add Timer Cleanup & Using Key Prop to Re-render

--- course/12 Demo Project - React Quiz/02-solution/src/components/QuestionTimer.jsx
+++ course/12 Demo Project - React Quiz/02-solution/src/components/QuestionTimer.jsx
@@ -4,15 +4,17 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    console.log('타임아웃 등록');
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => clearTimeout(timer);
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    console.log('인터벌 등록');
-    setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainigTime) => prevRemainigTime - 100);
     }, 100);
+
+    return () => clearInterval(interval);
   }, []);
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;
